38babe6739651226666fa2665efd99dcc732878a
thecircleisround <roundisthecircle@gmail.com>
2020-11-28 15:44:15 -0500

    setup sync

---
+++ hangman.py
@@ -0,0 +1,95 @@
+from random import seed
+from random import choice
+
+
+class Settings():
+    def __init__(self):
+        self.gameactive = True
+        self.chances = 10
+
+
+class Words():
+    def __init__(self):
+        self.worddisplay = ""
+        self.guesslist = []
+
+    def pickword(self):
+        while True:
+            categories = ["sports", "musicians", "states"]
+
+            print("Choose a category from the following list")
+            for category in categories:
+                print(category)
+            category = input("> ")
+            filename = (category + ".txt")
+            try:
+                f = open(filename, "r")
+                words = f.read().splitlines()
+                randomword = choice(words)
+                return randomword
+            except FileNotFoundError:
+                print("Not a valid category")
+
+
+def startgame(Settings):
+    settings = Settings()
+    words = Words()
+    while True:
+        word = words.pickword()
+        displayword(settings, words, word)
+        while settings.gameactive:
+            userguess(settings, words, word)
+            displayword(settings, words, word)
+            updatestatus(settings, words, word)
+
+        break
+
+
+def displayword(settings, words, word):
+    worddisplay = ""
+    for p in word:
+        if (p.lower() in words.guesslist or p == " "):
+            worddisplay += p
+        else:
+            worddisplay += "_ "
+    words.worddisplay = worddisplay
+    print(words.worddisplay)
+    print("Letters guessed so far " + str(words.guesslist))
+
+
+def userguess(settings, words, word):
+    userguess = input().lower()
+    for i in userguess:
+        words.guesslist.append(i)
+    words.guesslist = list(set(words.guesslist))
+    if userguess in word.lower():
+        print("You got one!")
+    else:
+        print("Not in this word. Try again!")
+        settings.chances -= 1
+        print(str(settings.chances) + " trys remaining")
+
+
+def updatestatus(settings, words, word):
+    if words.worddisplay == word:
+        print("You win!")
+        settings.gameactive = False
+    if settings.chances == 0:
+        print("You lose!")
+        print(settings.chances)
+        print("The answer was: " + word)
+        settings.gameactive = False
+
+
+def playagain():
+    playagain = input("Play again? (yes/no) ")
+    if playagain == "yes":
+        pass
+    else:
+        quit()
+
+
+while True:
+    startgame(Settings)
+    playagain()
+
