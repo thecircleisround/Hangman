788beffa9a2ce0055722f1ce0ca5a3abf96a715b
thecircleisround <roundisthecircle@gmail.com>
2020-12-01 14:55:05 -0500

    Incorporate basic user interface
    Use Pythonista ui module to incorporate basic user interface for
    hangman game. Really simple - module has limitations; granted it's not
    for game making.

--- hangman.py
+++ hangman.py
@@ -1,103 +1,134 @@
 from random import choice
 import ui
+from time import sleep
+
+reset = False
+
+def chooseword(userentry, showword):
+    choiceconfirmed = False
+    word = hangman.words.pickword(userentry, showword)
+    hangman.words.word = word
+    if word is not None: 
+        hangman.displayword(word, showword)
+        hangman.settings.gameactive = True
+
+
+def userentry(sender):
+    userentry = sender.superview['userentry']
+    showword = sender.superview['showword']
+    userentry.end_editing()
+    if userentry.text == "":
+        pass
+    else: 
+        if hangman.settings.gameactive != True:
+            chooseword(userentry, showword)
+            userentry.text = ""
+            userentry.begin_editing()
+        else:
+            word = hangman.words.word
+            hangman.userguess(word, userentry, showword)
+            hangman.displayword(word, showword)
+            hangman.updatestatus(word, showword)
+            userentry.text = ""
+    userentry.begin_editing()  
 
 def button_tapped(sender):
-    pass
+    sender.superview['submit'].text = "submit"
+    userentry(sender)
 
-def userentry(sender): 
-    view['showword'].text = "banana"
-    view['userentry'].text = ""
 
 view = ui.load_view('load_ui')
 
+
 class Settings():
     def __init__(self):
-        self.gameactive = True
+        self.gameactive = False
         self.chances = 10
+
 
 class Words():
     def __init__(self):
         self.worddisplay = ""
+        self.word = ""
         self.guesslist = []
         self.showword = view['showword']
 
-    def pickword(self):
-        while True:
-            categories = ["sports", "musicians", "states"]
+    def showcategories(self):
+        categories = ["sports", "musicians", "states"]
+        self.showword.text = "Choose a category from the following list"
+        for category in categories:
+            self.showword.text += "\n " + category
 
-            self.showword.text = "Choose a category from the following list"
-            for category in categories:
-                self.showword.text += "\n " + category
-            category = input("> ")
-            filename = (category + ".txt")
-            try:
-                f = open(filename, "r")
-                words = f.read().splitlines()
-                randomword = choice(words)
-                return randomword
-            except FileNotFoundError:
-                print("Not a valid category")
+    def pickword(self, userentry, showword):
+        filename = (userentry.text.lower() + ".txt")
+        try:
+            f = open(filename, "r")
+            words = f.read().splitlines()
+            randomword = choice(words)
+            return randomword
+        except FileNotFoundError:
+            pass
 
 class Hangman():
     def __init__(self):
-        self.words = Words()
-        self.settings = Settings()
-        
-        
-    def displayword(self, word):
+        self.newgame()
+
+    def displayword(self, word, showword):
         worddisplay = ""
-        
+
         for p in word:
             if (p.lower() in self.words.guesslist or p == " "):
                 worddisplay += p
             else:
                 worddisplay += "_ "
             self.words.worddisplay = worddisplay
-        self.showword = self.words.worddisplay
-        print("Letters guessed so far " + str(self.words.guesslist))
-    
-    def userguess(self, word):
-        userguess = input().lower()
-        for i in userguess:
+        # print("Letters guessed so far " + str(self.words.guesslist))
+        showword.text = worddisplay
+
+    def userguess(self, word, userentry, showword):
+        for i in userentry.text:
             self.words.guesslist.append(i)
         self.words.guesslist = list(set(self.words.guesslist))
-        if userguess in word.lower():
-            print("You got one!\n")
+        if userentry.text in word.lower():
+            #print("You got one!\n")
+            pass
         else:
-            print("Not in this word. Try again!")
-            self.settings.chances -= len(userguess) 
-            print(str(self.settings.chances) + " trys remaining\n")
-    
-    def updatestatus(self, word):
+            #print("Not in this word. Try again!")
+            self.settings.chances -= len(userentry.text)
+            #print(str(self.settings.chances) + " trys remaining\n")
+
+    def updatestatus(self, word, showword):
         if self.words.worddisplay == word:
-            print("You win!")
+            showword.text = word
+            #showword.text = "You Win! Play again?"
             self.settings.gameactive = False
+            self.newgame()
+
         if self.settings.chances <= 0:
-            print("You lose!")
-            print("The answer was: " + word)
+            showword.text = "You lose!"
+            #showword.text = "The answer was: " + word
             self.settings.gameactive = False
-        
-    def startgame(self):
-        while True:
-            word = self.words.pickword()
-            self.displayword(word)
-            while self.settings.gameactive:
-                self.userguess(word)
-                self.displayword(word)
-                self.updatestatus(word)
-            break
+            self.newgame()
             
-def playagain():
-    playagain = input("\nPlay again? (yes/no) ")
-    if playagain == "yes":
-        pass
-    else:
-        quit()
+    def newgame(self): 
+        self.settings = Settings()
+        self.words = Words()
+        self.words.showcategories()
+
+    def playagain(userentry,showword):
+        showword.text = "Play again?" 
+        if userentry == "yes":
+            self.newgame()
+        else:
+            quit()
+
 
 view.present('sheet')
-while True: 
-    hangman = Hangman()
-    hangman.startgame()
-    playagain()
-    
-    
+
+
+
+hangman = Hangman() 
+
+#hangman.startgame()
+#playagain()
+
